a714e7a976af5a940701780cdd26b0cd460af957
realugi <u.jovanovic@teletronic.at>
2021-10-25 10:26:31 +0200

    Initial commit

---
+++ database/seeders/TodoSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class TodoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Todo::factory()
+            ->count(50)
+            ->create();
+    }
+}
